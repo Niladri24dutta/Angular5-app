5b810823409efe0c0130c41daf19adc81821d263
Niladri28dutta <nild2141@gmail.com>
2017-12-04 22:41:51 +0530

    updated for ngStyle

--- Angular5Demo/src/app/stylebinding/stylebinding.component.ts
+++ Angular5Demo/src/app/stylebinding/stylebinding.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StylebindingComponent implements OnInit {
 
+  public availableStyles = {
+    color:"blue",
+    fontStyle:"italic"
+  } 
   constructor() { }
 
   ngOnInit() {
