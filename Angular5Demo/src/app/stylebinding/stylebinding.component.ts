00f40b5a99c64dc2193e0397031c18d8647cb47c
Niladri28dutta <nild2141@gmail.com>
2017-12-04 21:36:52 +0530

    added example for ngClass and ngStyle

---
+++ Angular5Demo/src/app/stylebinding/stylebinding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stylebinding',
+  templateUrl: './stylebinding.component.html',
+  styleUrls: ['./stylebinding.component.css']
+})
+export class StylebindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
